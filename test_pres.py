05550ec274d4ef3897d3cca5d6fcba7e1254c2cc
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2024-11-28 18:33:05 +0200

    Add files via upload

--- test_pres.py
+++ test_pres.py
@@ -3,30 +3,16 @@ from sklearn.metrics import mean_absolute_percentage_error,mean_absolute_error
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-sensor_mean = pd.read_csv('sensor_mean')
-
-sensor_mean = sensor_mean[['s2', 's3','s4']]
-
-dmg_perc = pd.read_csv('dmg_perc')
-
-dmg_perc = dmg_perc[['Damage_percentage']]
-
-damage_list = []
 
 
-print(len(dmg_perc['Damage_percentage']))
 
-
-for i in range(0,len(dmg_perc['Damage_percentage'])):
-    #damage_list.append(str(dmg_perc['Damage_percentage'][i]))
-    damage_list.append(dmg_perc['Damage_percentage'][i])
-
-
+##### kanw train me to original dataset
+from y_set_creator_dmg_percentage import y_set_creator
 from feature_vector import X,X_fft
 
 
 X = X_fft #X,X_fft,sensor_mean
-y = damage_list
+y = y_set_creator('Damage_percentage','regression')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,shuffle=True)
 
@@ -36,28 +22,25 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 
-
-#########kanw test me tis nees times pou edwse o rekatsinas
+############ kalw diafora montela############
 
 
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
-#lr.fit(X_fft,y)
+lr.fit(X_fft,y)
 
-from feature_vector_dokimes import X_dokimes,X_fft_dokimes
-from y_dokimes import damage_data_df
+
 import xgboost as xgb
-
 xgb = xgb.XGBRegressor()
-#xgb.fit(X,y)
+xgb.fit(X,y)
 
 from sklearn.svm import SVR
 svr = SVR()
-#svr.fit(X,y)
+svr.fit(X,y)
 
 from sklearn.tree import DecisionTreeRegressor
 dt = DecisionTreeRegressor() 
-#dt.fit(X,y)
+dt.fit(X,y)
 
 
 from sklearn.linear_model import Lasso
@@ -67,7 +50,7 @@ lasso = Lasso(alpha=0.000000002,
               tol=1.496e-07,
               selection='random'
               )
-#lasso.fit(X,y)
+lasso.fit(X,y)
 
 from sklearn.linear_model import ElasticNet
 
@@ -79,16 +62,47 @@ en = ElasticNet(alpha=0.000000005,
                 )
 en.fit(X,y)
 
+from sklearn.linear_model import Ridge
+
+rr = Ridge(
+        alpha=0.000000005,   
+        max_iter=1000000, 
+        tol=0.0000001, 
+        solver='auto',  
+        )
+rr.fit(X,y)
+################################################
 
 
 
-from sklearn.model_selection import cross_val_predict,LeaveOneOut,cross_val_score
+################ PCA ################
+
+
+
+
+
+
+
+################################################
+
+
+
+
+##### kanw test me times rek
+
+
+from feature_vector_dokimes_sample_rek import X_dokimes,X_fft_dokimes
+from y_dokimes import damage_data_df
+
 X = X_fft_dokimes
-y = damage_data_df
-y_pred = en.predict(X)
-y_true = [0.2,0.034,0.062,0.086,0.012]
+
+y_pred = rr.predict(X)
+print(y_pred)
+
+y_true = [0.02,0.034,0.062,0.086,0.12]
+
+
 print('mape is')
 print(mean_absolute_percentage_error(y_true,y_pred))
 print('mae is')
 print(mean_absolute_error(y_true,y_pred))
-print(y_pred)
