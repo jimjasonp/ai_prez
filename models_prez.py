207beb1424d56a53461e643c01a0d1667744a837
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2024-12-01 20:58:13 +0200

    Add files via upload

--- models_prez.py
+++ models_prez.py
@@ -18,6 +18,7 @@ from feature_vector import sensor_mean,sensor_max,sensor_stdev,sensor_median_hig
 
 
 
+'''
 X_train = pd.DataFrame({
     's2_mean':sensor_mean['s2'],
     's3_mean':sensor_mean['s3'],
@@ -35,56 +36,71 @@ X_train = pd.DataFrame({
     's3_stdev':sensor_stdev['s3'],
     's4_stdev':sensor_stdev['s4'],
     })
+'''
 
 
-
-#X_train = X #X,X_fft,sensor_mean
+X_train = X_fft #X,X_fft,sensor_mean
 y = y_set_creator('Damage_percentage','regression')
 
-scaler = StandardScaler()
-X_train= scaler.fit_transform(X_train)
+#scaler = StandardScaler()
+#X_train= scaler.fit_transform(X_train)
 
-X_train, X_drop, y, y_drop = train_test_split(X_train, y, test_size=0.01,shuffle=True)
+X_train, X_drop, y, y_drop = train_test_split(X_train, y, test_size=0.5,shuffle=True)
 
-X_train = pd.DataFrame(X_train)
+
+#X_train = pd.DataFrame(X_train)
 
 #################   RFECV   ##########################
 
 from sklearn.feature_selection import RFE
 from sklearn.tree import DecisionTreeRegressor
 rfe = RFE(estimator=DecisionTreeRegressor(), n_features_to_select = 3 )
-rfe.fit(X_train,y)
+#rfe.fit(X_train,y)
 #for i,col in zip(range(X_train.shape[1]), X_train.columns):
 #    print(f"{col} selected = {rfe.support_[i]} rank = {rfe.ranking_[i]}")
 
 
-X_train = pd.DataFrame({'feature1':X_train[4],'feature2':X_train[11],'feature3':X_train[10]})
+#X_train = pd.DataFrame({'feature1':X_train[4],'feature2':X_train[10],'feature3':X_train[1]})
 
 
+
+'''
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(projection='3d')
+
+sequence_containing_x_vals = X_train['feature1']
+sequence_containing_y_vals = X_train['feature2']
+sequence_containing_z_vals = X_train['feature3']
+
+ax.scatter(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals)
+plt.show()
+'''
 ###############################################
 
 
 
-'''
 #################     PCA     #####################
-from sklearn.decomposition import PCA
+#from sklearn.decomposition import PCA
 
-pca = PCA(n_components=0.7, random_state = 42)
-pca.fit(X_train)
+#pca = PCA(n_components=0.7, random_state = 42)
+#pca.fit(X_train)
 #X_train = pca.transform(X_train)
-X_train = pd.DataFrame(X_train)
+#X_train = pd.DataFrame(X_train)
 
 #The following code constructs the Scree plot
-per_var = np.round(pca.explained_variance_ratio_* 100, decimals=1)
-labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
+#per_var = np.round(pca.explained_variance_ratio_* 100, decimals=1)
+#labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
  
-plt.bar(x=range(1,len(per_var)+1), height=per_var, tick_label=labels)
-plt.ylabel('Percentage of Explained Variance')
-plt.xlabel('Principal Component')
-plt.title('Scree Plot')
+#plt.bar(x=range(1,len(per_var)+1), height=per_var, tick_label=labels)
+#plt.ylabel('Percentage of Explained Variance')
+#plt.xlabel('Principal Component')
+#plt.title('Scree Plot')
 #plt.show()
+
+
 ################################################
-'''
+
 
 
 
@@ -100,6 +116,8 @@ from y_dokimes import damage_data_df
 
 
 
+
+'''
 X_test = pd.DataFrame({
     's2_mean':sensor_mean['s2'],
     's3_mean':sensor_mean['s3'],
@@ -117,15 +135,15 @@ X_test = pd.DataFrame({
     's3_stdev':sensor_stdev['s3'],
     's4_stdev':sensor_stdev['s4'],
     })
+'''
 
+X_test = X_fft_dokimes
 
-#X_test = X_dokimes
+#X_test = scaler.transform(X_test)
+#X_test = pd.DataFrame(X_test)
 
-X_test = scaler.transform(X_test)
-X_test = pd.DataFrame(X_test)
-
-rfe.transform(X_test)
-X_test = pd.DataFrame({'feature1':X_test[4],'feature2':X_test[11],'feature3':X_test[10]})
+#rfe.transform(X_test)
+#X_test = pd.DataFrame({'feature1':X_test[4],'feature2':X_test[10],'feature3':X_test[1]})
 #X_test = pca.transform(X_test)
 
 
@@ -179,7 +197,7 @@ rr = Ridge(
 rr.fit(X_train,y)
 
 
-'''
+
 import tensorflow as tf
 
 from tensorflow import keras
@@ -189,30 +207,31 @@ from keras.models import Sequential
 from keras.layers import Flatten,Dense
 
 
-model = Sequential([
+model = Sequential()
     # Flatten input from 28x28 images to 784 (28*28) vector
-    Flatten(input_shape=(148, 3)),
+#model.add(Flatten(input_shape=(None, 2250)))
   
     # Dense layer 1 (256 neurons)
-    Dense(256, activation='sigmoid'),  
+model.add(Dense(256, activation='sigmoid'))
   
     # Dense layer 2 (128 neurons)
-    Dense(128, activation='sigmoid'), 
+model.add(Dense(128, activation='sigmoid'))
   
     # Output layer (10 classes)
-    Dense(10, activation='sigmoid'),  
+model.add(Dense(10, activation='sigmoid'))
 
-    Dense(1, activation='sigmoid'), 
-])
+model.add(Dense(1, activation='linear'))
+
 
 model.compile(loss="mean_squared_error", optimizer="sgd")
 
-history = model.fit(X_train, y, epochs=20)
+history = model.fit(X_train, y, epochs=15)
 
 ################################################
-'''
 
-y_pred = rr.predict(X_test)
+
+
+y_pred = model.predict(X_test)
 print(y_pred)
 
 y_true = [0.02,0.034,0.062,0.086,0.12
