bf2cbd02b61173c9b7af48103ea936c447f0e21b
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2024-12-09 21:26:24 +0200

    Add files via upload

--- test_transformations.py
+++ test_transformations.py
@@ -2,7 +2,7 @@ import numpy as np
 from test_file_opener import y_set,X_set
 from scipy import signal
 import matplotlib.pyplot as plt
-import pywt
+
 X_train = np.concatenate((
                         X_set(r'C:\Users\jimja\Desktop\thesis\random_data','none'),
                         X_set(r'C:\Users\jimja\Desktop\thesis\data','none')),
@@ -14,9 +14,29 @@ y = np.concatenate((
                         y_set(r'C:\Users\jimja\Desktop\thesis\data')),
                         axis=0)
 X_test = X_set(r'C:\Users\jimja\Desktop\thesis\dokimes','none')
+sample = X_train[0]
 
+def welch(sample):
+    fs = 1000
+    (f, S)= signal.welch(sample, fs, nperseg=512)
 
-coeffs =  pywt.dwt2(X_train, 'bior1.3')
-LL, (LH, HL, HH) = coeffs
+    plt.semilogy(f, S)
+    plt.xlim([0, 500])
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
 
-print(len(y))
+def psd(sample):
+    fs = 1000
+    # f contains the frequency components
+    # S is the PSD
+    (f, S) = signal.periodogram(sample, fs, scaling='density')
+
+    plt.semilogy(f, S)
+    #plt.ylim([1e-7, 1e2])
+    plt.xlim([0,1000])
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
+
+welch(sample)
